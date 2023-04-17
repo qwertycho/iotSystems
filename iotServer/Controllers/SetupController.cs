fc317bcc4d21bad306048c68097741b0697ea04b
qwertycho <tychovopstal@gmail.com>
2023-04-17 16:40:46 +0200

    werk aans setups

--- iotServer/Controllers/SetupController.cs
+++ iotServer/Controllers/SetupController.cs
@@ -7,6 +7,7 @@ namespace iotServer.Controllers
 {
     public class SetupController : Controller
     {
+
         private readonly ILogger<SetupController> _logger;
         private SetupModel setupModel;
 
@@ -15,10 +16,52 @@ namespace iotServer.Controllers
             _logger = logger;
             setupModel = new SetupModel();
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            _logger.LogInformation("Index page says hello");
+            int id = Convert.ToInt32(Request.Query["id"]);
+            DeviceSetup setup = await setupModel.GetDeviceSetupAsync(id);
+
+            ViewData["id"] = setup.id;
+            ViewData["aanTijd"] = setup.aanTijd;
+            ViewData["uitTijd"] = setup.uitTijd;
+            ViewData["maxTemp"] = setup.maxTemp;
+            ViewData["minTemp"] = setup.minTemp;
+
             return View();
+        }
+
+        public async Task<IActionResult> Save()
+        {
+
+            var form = HttpContext.Request.Form;
+
+            DeviceSetup setup = new DeviceSetup();
+            try
+            {
+                setup.id = Convert.ToInt32(form["id"]);
+                setup.aanTijd = Convert.ToInt32(form["aanTijd"]);
+                setup.uitTijd = Convert.ToInt32(form["uitTijd"]);
+                setup.maxTemp = float.Parse(form["maxTemp"]);
+                setup.minTemp = float.Parse(form["minTemp"]);
+
+                Console.WriteLine(setup.id);
+                Console.WriteLine(setup.aanTijd);
+                Console.WriteLine(setup.uitTijd);
+                Console.WriteLine(setup.maxTemp);
+                Console.WriteLine(setup.minTemp);
+
+                await setupModel.SaveSetupAsync(setup);
+
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+                // return View();
+            }
+
+
+
+            return Redirect($"Index?id={form["id"]}");
         }
 
         public async Task<JsonResult> GetSetup()
--- iotServer/Models/SetupModel.cs
+++ iotServer/Models/SetupModel.cs
@@ -12,6 +12,48 @@ namespace iotServer.classes
             builder = EnvParser.ConnectionStringBuilder();
         }
 
+        public async Task<DeviceSetup> GetDeviceSetupAsync(int id)
+        {
+            using var connection = new MySqlConnection(builder.ConnectionString);
+            await connection.OpenAsync();
+
+            using var command = new MySqlCommand("SELECT setupID, aanTijd, uitTijd, maxTemp, minTemp FROM devices INNER JOIN setup ON devices.setup = setup.setupID WHERE devices.deviceID = @id", connection);
+            command.Parameters.AddWithValue("@id", id);
+
+            using var reader = await command.ExecuteReaderAsync();
+
+            DeviceSetup setup = new DeviceSetup();
+
+
+            if (!reader.HasRows)
+            {
+                connection.Close();
+                return setup;
+            }
+            else
+            {
+
+                while (await reader.ReadAsync())
+                {
+                    setup.id = reader.GetInt32(0);
+                    setup.aanTijd = reader.GetInt32(1);
+                    setup.uitTijd = reader.GetInt32(2);
+                    setup.maxTemp = reader.GetFloat(3);
+                    setup.minTemp = reader.GetFloat(4);
+                }
+            }
+
+            connection.Close();
+
+            return setup;
+        }
+
+        /// <summary>
+        /// Haal alle setups op uit de database
+        /// </summary>
+        /// <returns>
+        /// List<DeviceSetup>
+        ///</returns>
         public async Task<List<DeviceSetup>> GetSetupsAsync()
         {
             using var connection = new MySqlConnection(builder.ConnectionString);
@@ -35,7 +77,46 @@ namespace iotServer.classes
                 setups.Add(setup);
             }
 
+            connection.Close();
+
             return setups;
+        }
+
+// updaten zodat id van device word gebruikt met een inner join
+        public async Task SaveSetupAsync(DeviceSetup setup)
+        {
+            using var connection = new MySqlConnection(builder.ConnectionString);
+            await connection.OpenAsync();
+
+            using var command = new MySqlCommand(@"
+            IF NOT EXISTS (SELECT * FROM setup WHERE setupID = @id) 
+            THEN
+                INSERT INTO setup (aanTijd, uitTijd, maxTemp, minTemp)
+                VALUES (@aanTijd, @uitTijd, @maxTemp, @minTemp);
+
+                UPDATE devices
+                SET setup = 1
+                WHERE deviceID = @id;
+
+            ELSE
+                UPDATE setup
+                SET aanTijd = @aanTijd, 
+                uitTijd = @uitTijd, 
+                maxTemp = @maxTemp, 
+                minTemp = @minTemp
+                WHERE setupID = @id;
+            END IF
+            ", connection);
+
+            command.Parameters.AddWithValue("@id", setup.id);
+            command.Parameters.AddWithValue("@aanTijd", setup.aanTijd);
+            command.Parameters.AddWithValue("@uitTijd", setup.uitTijd);
+            command.Parameters.AddWithValue("@maxTemp", setup.maxTemp);
+            command.Parameters.AddWithValue("@minTemp", setup.minTemp);
+
+            await command.ExecuteNonQueryAsync();
+
+            connection.Close();
         }
 
     }
