44dae95a2383922967f9899baabb0f61a07855c9
qwertycho <tychovopstal@gmail.com>
2023-05-24 13:21:46 +0200

    senscontrol

--- iotServer/Controllers/SensorController.cs
+++ iotServer/Controllers/SensorController.cs
@@ -10,34 +10,22 @@ namespace iotServer.Controllers
   {
     private readonly ILogger<SensorController> _logger;
     private readonly NewsLetter.NewsLetter _newsLetter;
-    private DeviceModel deviceModel;    
-//    private SensorModel sensorModel;
+    private DeviceModel deviceModel = new DeviceModel();    
+    //    private SensorModel sensorModel = new SensorModel();
 
     public SensorController(ILogger<SensorController> logger, NewsLetter.NewsLetter newsLetter)
     {
       _logger = logger;
       _newsLetter = newsLetter;
     }
+      
 
-    public async Task<JsonResult> insert([FromBody] List<SensorValue> sensorValues)
+    public async Task<JsonResult> updateTemp([FromBody] SensorValue sensorValue)
     {
-      Device device = await deviceModel.GetDeviceByID(sensorValues[0].id);
+      
 
-      return Json("ding");
-
-      for(int i = 0; i < sensorValues.Count(); i++)
-      {
-        try
-        {
-//          await sensorModel.addValue(sensorValues[i]);
-  //        _newsLetter.sensorUpdate(new SensorUpdateEventArgs { Message = sensorValues[i]});
-          return Json("ding");
-        } catch(Exception e)
-        {
-          _logger.LogError(e.Message);
-          return Json("ding");
-        }
-      }
+      return Json("lsp ding");
     }
+
   }
 }
