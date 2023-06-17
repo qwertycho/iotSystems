7cd0be9423776d491dc4a6d8f44fd64ee64d97c1
qwertycho <tychovopstal@gmail.com>
2023-06-17 16:16:40 +0200

    cors

--- iotServer/Program.cs
+++ iotServer/Program.cs
@@ -20,6 +20,17 @@ namespace iotServer
             // dependency injection
             builder.Services.AddSingleton<NewsLetter.NewsLetter>();
 
+            // allow cors
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", builder =>
+                {
+                    builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                });
+            });
+
 
             var app = builder.Build();
 
