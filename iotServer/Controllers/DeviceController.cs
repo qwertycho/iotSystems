e48936344bdbc47081598169c2216a761f4d8a75
qwertycho <tychovopstal@gmail.com>
2023-06-18 14:09:59 +0200

    foutje

--- iotServer/Controllers/DeviceController.cs
+++ iotServer/Controllers/DeviceController.cs
@@ -61,11 +61,12 @@ namespace iotServer.Controllers
                 deviceModel.validateNewDevice(device);
                 DeviceSetup deviceSetup = await deviceModel.initDevice(device);
 
-                if(deviceSetup.id != 0)
+                if(deviceSetup.id == 0)
                 {
+                  await deviceModel.setStatus(device.Uuid, "M");
+                } else{
                   await deviceModel.setStatus(device.Uuid, "A");
                 }
-                  await deviceModel.setStatus(device.Uuid, "N");
 
                 return Json(deviceSetup);
             }
